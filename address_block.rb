ba8fc4083e3c5e219434836a5f114ce14090655a
George Bumgardner <bumgardnera07@gmail.com>
2017-08-15 02:10:46 +0000

    controller is working

--- address_block.rb
+++ address_block.rb
@@ -1,1 +1,8 @@
-puts "Welcome to AddressBloc!"
+ require_relative 'controllers/menu_controllers'
+ 
+  menu = MenuController.new
+ 
+ system "clear"
+ puts "Welcome to AddressBloc!"
+ 
+  menu.main_menu
